dd94321f29e2df8d6167ee26d954fbc4955e7061
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-08-17 19:37:35 +0900

    結合テストの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,7 @@ class MessagesController < ApplicationController
   end
 
   def create
+    
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
